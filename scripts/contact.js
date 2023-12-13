9b9f1ffa14e111905ec6b37c637508bf3fbf8bb9
OwenC03 <152634448+OwenC03@users.noreply.github.com>
2023-12-12 22:04:08 -0700

    validator changes pt2

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,15 +1,15 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 var submit_button = document.getElementById("submit-button");
-var page = document.getElementById("contact-page")
+var page = document.getElementById("contact-page");
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 function submit_page() {
     var newMessage = document.createElement("p");
-    newMessage.textContent = "Thank you for your message!"
-    newMessage.style.fontSize = "24px"
+    newMessage.textContent = "Thank you for your message!";
+    newMessage.style.fontSize = "24px";
 
     while(page.firstChild) {
         page.removeChild(page.firstChild);
     }
     page.appendChild(newMessage);
 }
-submit_button.addEventListener("click", submit_page)
+submit_button.addEventListener("click", submit_page);
